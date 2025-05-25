e754a8ae952348dfb95a3852bb2059ea7b69d1f8
Luiz Henrique <luizhenriqueee31@gmail.com>
2025-05-25 20:22:19 -0300

    feat: add login and registration screens

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { styles } from "../styles/LoginScreenStyle";
+import { useFonts } from "expo-font";
+import { Poppins_600SemiBold, Poppins_500Medium, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import { Ionicons } from "@expo/vector-icons";
 
-export default function LoginScreen ({navigation}) {
-    
+export default function LoginScreen({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const [fontsLoaded] = useFonts({
+        Poppins_500Medium,
+        Poppins_600SemiBold,
+        Poppins_400Regular,
+        Poppins_700Bold,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container}>
+
+            <TouchableOpacity onPress={() => navigation.goBack('Welcome')} style={styles.backButton}>
+                <Ionicons  name="arrow-back" size={24} style={styles.backButtonIcon}/>
+            </TouchableOpacity>
+
+            <Text style={styles.title}>Faça login na sua conta</Text>
+
+            <Text style={styles.label}>E-mail</Text>
+            <TextInput
+                style={styles.input}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+            />
+
+            <Text style={styles.label}>Senha</Text>
+            <View style={styles.passwordContainer}>
+            <TextInput
+                style={[styles.input, {flex: 1}]}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={!showPassword}
+            />
+            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                <Ionicons
+                    name={showPassword ? "eye-off" : "eye"}
+                    style={styles.eyeIcon}
+                    size={24}
+                />
+            </TouchableOpacity>
+            </View>
+
+            <View style={styles.forgotOption}>
+                <TouchableOpacity>
+                    <Text style={styles.forgotText}>Esqueci minha senha</Text>
+                </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity 
+                style={styles.loginButton}
+                onPress={() => navigation.navigate('Home')}
+                >
+                <Text style={styles.loginText}>Login</Text>
+            </TouchableOpacity>
+
+            <Text style={styles.register}>
+                Não tem uma conta?
+                <Text style={styles.registerLink} onPress={() => navigation.navigate('Register')}>{' '}Cadastrar-se</Text>
+            </Text>
+        </View>
+    )
 }
