bf71c974c4230670e55c110d8a7f6fd4f3d5d480
b.lorena2208@gmail.com <B.lorena2208@gmail.com>
2025-06-08 14:16:23 -0300

    add SafeAreaProvider to navigation

--- App.js
+++ App.js
@@ -2,33 +2,36 @@ import React, { useEffect } from "react";
 import { initDB } from "./src/services/database/db";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import WelcomeScreen from "./src/screens/WelcomeScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
 import ProfileUpdate from "./src/screens/ProfileUpdateScreen";
 import TabNavigator from "./src/components/TabNavigator";
-import CardsScreen from "./src/screens/CardsScreen"; 
-import RevisionScreen from "./src/screens/RevisionScreen"; 
-import NewCardScreen from "./src/screens/NewCardScreen"; 
+import CardsScreen from "./src/screens/CardsScreen";
+import RevisionScreen from "./src/screens/RevisionScreen";
+import NewCardScreen from "./src/screens/NewCardScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  
-  useEffect(() => { initDB();}, []);
-  
+
+  useEffect(() => { initDB(); }, []);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Welcome">
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="ProfileUpdate" component={ProfileUpdate} />
-        <Stack.Screen name="Home" component={TabNavigator} />
-        <Stack.Screen name="CardsScreen" component={CardsScreen} />
-        <Stack.Screen name="NewCard" component={NewCardScreen} />
-        <Stack.Screen name="RevisionScreen" component={RevisionScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Welcome">
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="ProfileUpdate" component={ProfileUpdate} />
+          <Stack.Screen name="Home" component={TabNavigator} />
+          <Stack.Screen name="CardsScreen" component={CardsScreen} />
+          <Stack.Screen name="NewCard" component={NewCardScreen} />
+          <Stack.Screen name="RevisionScreen" component={RevisionScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
